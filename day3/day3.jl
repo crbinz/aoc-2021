870f45f237d1b3a407c1e0e8dd27f59512decbd8
Chris Binz <chris.binz@gmail.com>
2021-12-12 23:04:10 -0500

    Finish Day 3

--- day3/day3.jl
+++ day3/day3.jl
@@ -1,3 +1,6 @@
+using DelimitedFiles
+
+#inputs = readdlm("test_input.txt",String);
 inputs = readdlm("input.txt",String);
 
 input_matrix = Array{Int64}(undef,length(inputs),length(inputs[1]))
@@ -19,7 +22,7 @@ end
 
 # Given a binary matrix, return a row vector of the most common bit for each column
 function most_common_bits(m)
-    sum(m,dims=1)./size(m)[1] .> 0.5
+    sum(m,dims=1)./size(m)[1] .>= 0.5
 end
 
 println("Gamma rate:")
@@ -31,4 +34,28 @@ erate = get_epsilon_rate(input_matrix)
 println(erate)
 
 println("Power level:")
-parse(Int,"0b"*grate) * parse(Int,"0b"*erate)
+println(parse(Int,"0b"*grate) * parse(Int,"0b"*erate))
+
+
+function filter(m; criteria=:most)
+    goodidx = collect(1:size(m,1))
+    i = 1
+    while length(goodidx) > 1
+        crit = criteria == :most ? most_common_bits(input_matrix[goodidx,i]) : .!(most_common_bits(input_matrix[goodidx,i]))
+        goodidx2 = findall(input_matrix[:,i] .== Int.(crit))
+        goodidx = intersect(goodidx,goodidx2)
+        i = i+1
+    end
+    return join(Int.(input_matrix[goodidx,:]))
+end
+
+println("Oxygen generator rating:")
+o2rate = parse(Int,"0b"*filter(input_matrix,criteria=:most))
+println(o2rate)
+
+println("CO2 scrubber rating:")
+co2scrubber_rate = parse(Int,"0b"*filter(input_matrix, criteria=:least))
+println(co2scrubber_rate)
+
+println("Life support rating:")
+println(o2rate*co2scrubber_rate)
