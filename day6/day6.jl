32b77f93d42cea86ce78aeecd6c254f9bf830a64
Chris Binz <chris.binz@gmail.com>
2021-12-28 22:34:49 -0500

    day 6 part b

--- day6/day6.jl
+++ day6/day6.jl
@@ -1,4 +1,4 @@
-using DelimitedFiles 
+using DelimitedFiles, ProgressBars
 
 mutable struct Lanternfish
     timer::Int64
@@ -31,9 +31,57 @@ end
 #fish = parse_inputs("test-input.txt")
 fish = parse_inputs("input.txt")
 
+lengths = zeros(80)
+
 for i = 1:80
     step!(fish) 
+    lengths[i] = length(fish)
 end
 
 println("Part 1:")
 println(length(fish))
+
+function step!(fishnumbers::Vector{Int64})
+    oldnumbers = copy(fishnumbers)
+    # 1
+    fishnumbers[2] = oldnumbers[3]
+    # 2
+    fishnumbers[3] = oldnumbers[4]
+    # 3
+    fishnumbers[4] = oldnumbers[5]
+    # 4
+    fishnumbers[5] = oldnumbers[6]
+    # 5
+    fishnumbers[6] = oldnumbers[7]
+    # 6
+    fishnumbers[7] = oldnumbers[1] + oldnumbers[8]
+    # 7
+    fishnumbers[8] = oldnumbers[9]
+    # 8
+    fishnumbers[9] = oldnumbers[1]
+    # 0
+    fishnumbers[1] = oldnumbers[2]
+
+    return fishnumbers
+end
+
+function parse_inputs_count(fn::String)
+    inputs = readdlm(fn,',',Int64)
+
+    numbers = zeros(Int64,9)
+
+    for i in 1:9
+        numbers[i] = count(inputs.==(i-1))
+    end
+    return numbers
+end
+
+#fishnums = parse_inputs_count("test-input.txt")
+fishnums = parse_inputs_count("input.txt")
+
+for i = 1:256
+    step!(fishnums)
+end
+
+println("Part 2:")
+println(sum(fishnums))
